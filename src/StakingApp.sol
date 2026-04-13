f8a6f593dbf779fe50c7804bc132e0b0b13f3a21
FacundoMaciel <facundomaciel12@hotmail.com>
2026-04-13 20:40:44 -0300

    Staking app functinalities and firsts tests added

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -3,13 +3,76 @@
 pragma solidity 0.8.24;
 
 import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+using SafeERC20 for IERC20;
 
 contract StakingApp is Ownable {
     address public stakingToken;
+    uint256 public stakingPerdiod;
+    uint256 public fixedStakingAmount;
+    uint256 public rewardPerPeriod;
+    mapping(address => uint256) public userBalance;
+    mapping(address => uint256) public elapsedTime;
 
-    constructor(address stakingToken_, address owner_) Ownable(owner_) {
+    event ChangeStakingPeriod(uint256 newStakingPeriod_);
+    event DepositToken(address userAddress_, uint256 depositAmount_);
+    event WithdrawToken(address userAddress_, uint256 withdrawAmount_);
+    event EtherSend(uint256 amount_);
+
+    constructor(
+        address stakingToken_,
+        address owner_,
+        uint256 stakingPerdiod_,
+        uint256 fixedStakingAmount_,
+        uint256 rewardPerPeriod_
+    ) Ownable(owner_) {
         stakingToken = stakingToken_;
+        stakingPerdiod = stakingPerdiod_;
+        fixedStakingAmount = fixedStakingAmount_;
+        rewardPerPeriod = rewardPerPeriod_;
     }
 
     // Functions
+
+    function deposit(uint256 tokenAmountToDeposit_) external {
+        require(tokenAmountToDeposit_ == fixedStakingAmount, "Only deposit fixed amount");
+        require(userBalance[msg.sender] == 0, "Already deposited");
+
+        IERC20(stakingToken).safeTransferFrom(msg.sender, address(this), tokenAmountToDeposit_);
+        userBalance[msg.sender] += tokenAmountToDeposit_;
+        elapsedTime[msg.sender] = block.timestamp;
+
+        emit DepositToken(msg.sender, tokenAmountToDeposit_);
+    }
+
+    function withdraw() external {
+        uint256 balance_ = userBalance[msg.sender];
+        userBalance[msg.sender] = 0;
+        IERC20(stakingToken).safeTransfer(msg.sender, balance_);
+
+        emit WithdrawToken(msg.sender, balance_);
+    }
+
+    function claimReward() external {
+        require(userBalance[msg.sender] == fixedStakingAmount, "No staking");
+
+        uint256 elapsedPeriod_ = block.timestamp - elapsedTime[msg.sender];
+        require(elapsedPeriod_ >= stakingPerdiod, "Not yet claimable");
+
+        elapsedTime[msg.sender] = block.timestamp;
+
+        (bool success_,) = msg.sender.call{value: rewardPerPeriod}("");
+        require(success_, "transfer failed");
+    }
+
+    receive() external payable onlyOwner {
+        emit EtherSend(msg.value);
+    }
+
+    function changeStakingPeriod(uint256 newStakingPeriod_) external onlyOwner {
+        stakingPerdiod = newStakingPeriod_;
+        emit ChangeStakingPeriod(newStakingPeriod_);
+    }
 }
---
+++ test/StakingAppTest.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import "forge-std/Test.sol";
+import "../src/StakingToken.sol";
+import "../src/StakingApp.sol";
+
+contract StakingAppTest is Test {
+    
+    StakingApp stakingApp;
+    StakingToken stakingToken;
+
+    // Token parameters
+    string name_ = "Staking Token";
+    string symbol_ = "STK";
+
+    // App parameters
+    address owner_ = vm.addr(1);
+    uint256 stakingPerdiod_ = 1 days;
+    uint256 fixedStakingAmount_ = 10 ether;
+    uint256 rewardPerPeriod_ = 1 ether;
+
+    
+    function setUp() public {
+        stakingToken = new StakingToken(name_, symbol_);
+        stakingApp = new StakingApp(address(stakingToken), owner_, stakingPerdiod_, fixedStakingAmount_, rewardPerPeriod_);
+    }
+
+    function testStakingTokenCorrectlyDeployed() external view {
+        assert(address(stakingToken) != address(0));
+    }
+
+    function testStakingAppCorrectlyDeployed() external view {
+        assert(address(stakingApp) != address(0));
+    }
+
+}
