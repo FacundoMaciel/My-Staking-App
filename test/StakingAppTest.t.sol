e7f323a03e5e94c22a8b94a6f6c2582498abe84e
FacundoMaciel <facundomaciel12@hotmail.com>
2026-04-14 20:19:51 -0300

    Add more testing for the Staking App

--- test/StakingAppTest.t.sol
+++ test/StakingAppTest.t.sol
@@ -20,6 +20,8 @@ contract StakingAppTest is Test {
     uint256 fixedStakingAmount_ = 10 ether;
     uint256 rewardPerPeriod_ = 1 ether;
 
+    address randomUser_ = vm.addr(2);
+
     function setUp() public {
         stakingToken = new StakingToken(name_, symbol_);
         stakingApp =
@@ -32,5 +34,40 @@ contract StakingAppTest is Test {
 
     function testStakingAppCorrectlyDeployed() external view {
         assert(address(stakingApp) != address(0));
+    }
+
+    function testShowRevertIfNotOwner() external {
+        uint256 newStakingPeriod_ = 1;
+
+        vm.expectRevert();
+        stakingApp.changeStakingPeriod(newStakingPeriod_);
+    }
+
+    function testShouldChangeStakingPeriod() external {
+        vm.startPrank(owner_);
+        uint256 newStakingPeriod_ = 1;
+
+        uint256 stakingPeriodBefore_ = stakingApp.stakingPerdiod();
+        stakingApp.changeStakingPeriod(newStakingPeriod_);
+        uint256 stakingPeriodAfter_ = stakingApp.stakingPerdiod();
+
+        assert(stakingPeriodBefore_ != newStakingPeriod_);
+        assert(stakingPeriodAfter_ == newStakingPeriod_);
+        vm.stopPrank();
+    }
+
+    function testContractReceiveEtherCorrectly() external {
+        vm.startPrank(owner_);
+        vm.deal(owner_, 1 ether);
+
+        uint256 etherValue_ = 1 ether;
+        uint256 balanceBefore_ = address(stakingApp).balance;
+        (bool success,) = address(stakingApp).call{value: etherValue_}("");
+        uint256 balanceAfter_ = address(stakingApp).balance;
+        require(success, "Failed to send");
+
+        assert(balanceAfter_ - balanceBefore_ == etherValue_);
+
+        vm.stopPrank();
     }
 }
